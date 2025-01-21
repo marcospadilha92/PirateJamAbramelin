dd8042581bc257bd0f86d43ae24eb3804bb4a0fc
unknown <marcospad92@gmail.com>
2025-01-21 20:02:20 -0300

    chore: animation player

--- Assets/Scripts/RigidbodyDragController.cs
+++ Assets/Scripts/RigidbodyDragController.cs
@@ -5,6 +5,9 @@ public class RigidbodyDragController : MonoBehaviour,
     IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     private Rigidbody2D body;
+    public Animator animator;
+
+    public float moveSpeed;
 
     public float velocityMultiplier = 20f;
     public float maxVelocity = 50f;
@@ -18,6 +21,27 @@ public class RigidbodyDragController : MonoBehaviour,
         body = GetComponent<Rigidbody2D>();
     }
 
+    // normal movement
+    void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+        {
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+            body.AddForce(
+                new Vector2(horizontal * moveSpeed * Time.deltaTime, 0));
+        }
+        else
+        {
+            body.AddForce(
+                new Vector2(0, vertical * moveSpeed * Time.deltaTime));
+            //animator.SetFloat("Run", Mathf.Abs(vertical));
+        }
+    }
+
     // drag events
     public void OnDrag(PointerEventData pointer)
     {
@@ -27,16 +51,18 @@ public class RigidbodyDragController : MonoBehaviour,
     public void OnBeginDrag(PointerEventData pointer)
     {
         isDragging = true;
+        animator.SetBool("IsSpin", true);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         isDragging = false;
+        animator.SetBool("IsSpin", false);
     }
 
     public void FixedUpdate()
     {
-        if(isDragging)
+        if (isDragging)
         {
             float displacement = velocityMultiplier * Time.fixedDeltaTime;
             Vector2 position = Vector2.Lerp(body.position, mousePosition, displacement);
@@ -49,6 +75,10 @@ public class RigidbodyDragController : MonoBehaviour,
                 velocity = velocity.normalized * maxVelocity;
             }
             body.linearVelocity = velocity;
+        }
+        if (!isDragging)
+        {
+            Movement();
         }
     }
 }
